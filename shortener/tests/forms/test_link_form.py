8ff79554be45f5624564b5055de0668015e40fbb
taha davari <tahadavari1381@gmail.com>
2024-01-03 12:47:00 +0330

    test form

--- shortener/tests/forms/test_link_form.py
+++ shortener/tests/forms/test_link_form.py
@@ -6,21 +6,17 @@ from shortener.forms.link_form import LinkForm
 class TestLinkForm(TestCase):
 
     def test_valid_link(self):
-        # اعتبارسنجی لینک معتبر
         form = LinkForm({'original_link': 'https://www.example.com/'})
-        self.assertTrue(form.is_valid())  # فرم باید معتبر باشد
+        self.assertTrue(form.is_valid())
 
     def test_invalid_link(self):
-        # اعتبارسنجی لینک نامعتبر
         form = LinkForm({'original_link': 'invalid_link'})
-        self.assertFalse(form.is_valid())  # فرم باید نامعتبر باشد
+        self.assertFalse(form.is_valid())
 
     def test_blank_link(self):
-        # بررسی ورودی خالی
         form = LinkForm({'original_link': ''})
-        self.assertFalse(form.is_valid())  # فرم باید نامعتبر باشد
+        self.assertFalse(form.is_valid())  
 
     def test_empty_data(self):
-        # بررسی ارسال داده‌های خالی
         form = LinkForm({})
-        self.assertFalse(form.is_valid())  # فرم باید نامعتبر باشد
+        self.assertFalse(form.is_valid())
